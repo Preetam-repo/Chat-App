848884376f1f5a3dffdab95fac5bdcea93919bc0
Preetam Kumar <preetam1610kumar@gmail.com>
2021-01-14 21:41:23 +0530

    added chat-message event listener

--- server/server.js
+++ server/server.js
@@ -27,7 +27,5 @@ io.on("connection",(socket) => {
 
 app.use(express.static(publicPath));
 
-
-
 const PORT = process.env.PORT || 3000;
 server.listen(PORT , () => console.log(`Connected To Server On PORT ${PORT}`));
